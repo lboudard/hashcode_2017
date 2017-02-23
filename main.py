5d80ff2d99a38b407d99e701e0437c071c35ce89
saraELAFIA <sara.elafia@artefact.is>
2017-02-23 17:15:07 +0100

    main is not emoty yeay

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+# -*- coding: utf-8 -*-
+import optparse
 
 
 def main():
-    print 'heyyyyy'
+
+    parser = optparse.OptionParser()
+    parser.add_option("-f", "--file", dest="filename",
+                      help="write report to FILE", default='requirements.txt')
+    (options, args) = parser.parse_args()
+    with open(options.filename) as f:
+        f.read()
 
 if __name__ == '__main__':
     main()
